c9f41a1af512f6ab7983758b05b8aba41009fae9
vinipx <vinipeixotofagundes@gmail.com>
2020-09-01 22:28:47 +0200

    PTBR- StructuralAdapter - Introduzindo classe com teste da aplicacao

---
+++ src/test/java/com/rmbraga/structural/adapter/AdapterTest.java
@@ -0,0 +1,10 @@
+package com.rmbraga.structural.adapter;
+
+import org.junit.jupiter.api.Test;
+
+public class AdapterTest {
+    @Test
+    public void adaptadorTest() {
+        Aplicacao.main(new String []{});
+    }
+}
